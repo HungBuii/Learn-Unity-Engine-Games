c452fa53dd1ff2b7066d85cc8488c6a5f7a49350
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-07-28 14:38:25 +0700

    update

---
+++ Laser Defender/Assets/Scripts/Health.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] bool isPlayer = false;
+    [SerializeField] int health = 50;
+    [SerializeField] int score = 50;
+    [SerializeField] ParticleSystem hitEffect;
+    [SerializeField] bool applyCameraShake = false;
+
+    CameraShake cameraShake;
+    AudioPlayer audioPlayer;
+    ScoreKeeper scoreKeeper;
+    LevelManager levelManager;
+
+    void Awake()
+    {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    /* 
+        case 1: Projectile(Enemy) or Enemy vs Player 
+        case 2: Projectile(Player) vs Enemy
+    */
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+
+        if (damageDealer != null)
+        {
+            TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
+            audioPlayer.PlayDamageClip();
+            ShakeCamera();
+            damageDealer.Hit();
+        }
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (!isPlayer) scoreKeeper.ModifyScore(score);
+        else levelManager.LoadGameOver();
+        Destroy(gameObject);
+    }
+
+    void PlayHitEffect()
+    {
+        if (hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
+    }
+
+    void ShakeCamera()
+    {
+        if (cameraShake != null && applyCameraShake)
+        {
+            cameraShake.Play();
+        }
+    }
+}
