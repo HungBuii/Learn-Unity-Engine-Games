1e4fcf7465cee41c6e96e11fbe3e353bdfd2ee97
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-07-28 14:39:20 +0700

    update

---
+++ Quiz Master/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulations!\nYou got a score of: " +
+                               scoreKeeper.CalculateScore() + "%";
+    }
+    
+}
